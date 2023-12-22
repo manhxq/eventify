65d3767803dcba9438463a00b8e188f69227ff6b
manhxq <154581557+manhxq@users.noreply.github.com>
2023-12-22 20:15:22 +0700

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,6 +1,9 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { clerkClient } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+import { createUser } from '@/lib/actions/user.actions'
  
 export async function POST(req: Request) {
  
