106f254edb5dd50c145c23c0637cd0d5d8d81fc1
manhxq <154581557+manhxq@users.noreply.github.com>
2023-12-22 20:14:20 +0700

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -51,30 +51,30 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
- if(eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
-
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      userName: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    }
-
-    const newUser = await createUser(user);
-
-    if(newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id
-        }
-      })
-    }
-
-    return NextResponse.json({ message: 'OK', user: newUser })
-  }
+  if(eventType === 'user.created') {
+     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+ 
+     const user = {
+       clerkId: id,
+       email: email_addresses[0].email_address,
+       userName: username!,
+       firstName: first_name,
+       lastName: last_name,
+       photo: image_url,
+     }
+ 
+     const newUser = await createUser(user);
+ 
+     if(newUser) {
+       await clerkClient.users.updateUserMetadata(id, {
+         publicMetadata: {
+           userId: newUser._id
+         }
+       })
+     }
+ 
+     return NextResponse.json({ message: 'OK', user: newUser })
+   }
  
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
   console.log('Webhook body:', body)
